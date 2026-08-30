5f3cc3e72663fcad80f0c94fdfe5f67b08a3aad2
JustSirius7 <pino.1503@libero.it>
2026-08-31 00:45:12 +0200

    Aggiunto login dedicato per le candidature con state=candidati

---
+++ netlify/functions/discord-login-candidati.js
@@ -0,0 +1,16 @@
+exports.handler = async (event, context) => {
+  const CLIENT_ID = "1525515077283876994";
+  const REDIRECT_URI = "https://lsracing.top/.netlify/functions/discord-callback";
+  const API_ENDPOINT = "https://discord.com/api/v10";
+
+  const discordUrl = `${API_ENDPOINT}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20guilds.members.read&state=candidati`;
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: discordUrl,
+      "Cache-Control": "no-cache"
+    },
+    body: ""
+  };
+};
